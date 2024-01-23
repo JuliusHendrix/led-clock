608008ca0c0bf05c58fd51ebae10f7588495f218
JuliusHendrix <jlam.hendrix@gmail.com>
2024-01-23 19:38:41 +0100

    application threading in base class

--- src/applications/application_abc.py
+++ src/applications/application_abc.py
@@ -1,9 +1,61 @@
 from abc import ABC
+import os
+from pathlib import Path
+import sys
+from threading import Thread, Lock
+import time
+
+# own modules
+script_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = str(Path(script_dir).parents[0])
+src_dir = os.path.join(root_dir, "src")
+
+sys.path.append(src_dir)
+
+from matrices.frame_manager import FrameManager
 
 
 class ApplicationABC(ABC):
+    def __init__(self, refresh_rate: float) -> None:
+        super().__init__()
+
+        self._period = 1 / refresh_rate
+        self._thread = Thread(target=self._clock_loop)
+        self._thread_running = False
+        self._thread_stop_request = False
+        self._thread_mutex = Lock()
+
+    def _clock_loop(self) -> None:
+        next_call = time.time()
+        stop_request = False
+        while not stop_request:
+            with self._thread_mutex:
+                stop_request = self._thread_stop_request
+            self._update()
+            next_call = next_call + self._period
+            print(next_call)
+            time.sleep(next_call - time.time())
+
     def start(self) -> bool:
-        raise NotImplementedError()
+        if self._thread_running:
+            return False
+
+        self._thread.start()
+        self._thread_running = True
+
+        return True
 
     def stop(self) -> bool:
+        if not self._thread_running:
+            return False
+
+        with self._thread_mutex:
+            self._thread_stop_request = True
+
+        self._thread.join()
+        self._thread_stop_request = False
+
+        return True
+
+    def _update(self) -> None:
         raise NotImplementedError()
--- src/applications/clock_application.py
+++ src/applications/clock_application.py
@@ -3,8 +3,6 @@ from pathlib import Path
 import sys
 import numpy as np
 from datetime import datetime
-from threading import Thread, Lock
-import time
 
 
 # own modules
@@ -22,18 +20,12 @@ from patterns.string.fonts.clock_font import ClockFont
 
 class ClockApplication(ApplicationABC):
     def __init__(self, frame_manager: FrameManager) -> None:
-        super().__init__()
+        super().__init__(2)
 
         self.frame_manager = frame_manager
         self.string_converter = StringConverter(ClockFont())
 
-        self.refresh_rate = 1  # Hz
-        self.clock_thread = Thread(target=self.clock_loop)
-        self.thread_running = False
-        self.thread_stop_request = False
-        self.thread_mutex = Lock()
-
-    def update_time(self) -> None:
+    def _update(self) -> None:
         current_time_string = datetime.now().strftime("%H:%M")
 
         print(current_time_string)
@@ -46,35 +38,3 @@ class ClockApplication(ApplicationABC):
         full_array[1:15, :, :] = time_string_array
 
         self.frame_manager.queue_frame(full_array)
-
-    def clock_loop(self) -> None:
-        next_call = time.time()
-        stop_request = False
-        while not stop_request:
-            with self.thread_mutex:
-                stop_request = self.thread_stop_request
-            self.update_time()
-            next_call = next_call + 1
-            print(next_call)
-            time.sleep(next_call - time.time())
-
-    def start(self) -> bool:
-        if self.thread_running:
-            return False
-
-        self.clock_thread.start()
-        self.thread_running = True
-
-        return True
-
-    def stop(self) -> bool:
-        if not self.thread_running:
-            return False
-
-        with self.thread_mutex:
-            self.thread_stop_request = True
-
-        self.clock_thread.join()
-        self.thread_stop_request = False
-
-        return True
